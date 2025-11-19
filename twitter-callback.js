e801cf24547aaa8409a81a63ba80ac60f2d89394
Bajci2 <peti0618@gmail.com>
2025-11-19 20:17:22 +0000

    Update twitter-callback.js

--- twitter-callback.js
+++ twitter-callback.js
@@ -12,14 +12,14 @@ export default async function handler(req, res) {
     return res.status(200).json({
       ok: true,
       message: "Twitter callback működik!",
-      code,
-      state
+      code: code,
+      state: state
     });
 
-  } catch (e) {
+  } catch (error) {
     return res.status(500).json({
       ok: false,
-      error: e.message
+      error: error.message
     });
   }
 }
