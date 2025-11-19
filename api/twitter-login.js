d3e9a939fe98ea84f2399ccb47fc1f6df170087b
Bajci2 <peti0618@gmail.com>
2025-11-19 19:37:35 +0000

    Update twitter-login.js

--- api/twitter-login.js
+++ api/twitter-login.js
@@ -1,6 +1,16 @@
 export default function handler(req, res) {
-  res.status(200).json({
-    ok: true,
-    message: "Twitter login API működik (teszt).",
-  });
+  const TWITTER_CLIENT_ID = process.env.TWITTER_CLIENT_ID;
+  const REDIRECT_URI = "https://catkitsune.xyz/api/twitter-callback";
+
+  const url =
+    "https://twitter.com/i/oauth2/authorize" +
+    "?response_type=code" +
+    "&client_id=" + TWITTER_CLIENT_ID +
+    "&redirect_uri=" + encodeURIComponent(REDIRECT_URI) +
+    "&scope=users.read%20tweet.read%20tweet.write%20offline.access" +
+    "&state=login" +
+    "&code_challenge=challenge" +
+    "&code_challenge_method=plain";
+
+  return res.redirect(url);
 }
