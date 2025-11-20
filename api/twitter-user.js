b0ebe2c0a04a8b6b26970b092b7a3c21fca974d3
Bajci2 <peti0618@gmail.com>
2025-11-20 19:50:19 +0000

    Update twitter-user.js

--- api/twitter-user.js
+++ api/twitter-user.js
@@ -1,34 +1,18 @@
 export default async function handler(req, res) {
-  const { access_token } = req.query;
+  const { token } = req.query;
 
-  if (!access_token) {
-    return res.status(400).json({ ok: false, error: "Missing access_token" });
+  if (!token) {
+    return res.status(400).json({ error: "Missing token" });
   }
 
   try {
     const response = await fetch("https://api.twitter.com/2/users/me", {
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-      },
+      headers: { Authorization: `Bearer ${token}` },
     });
 
     const data = await response.json();
-
-    if (data.errors) {
-      return res.status(400).json({ ok: false, error: data.errors });
-    }
-
-    return res.status(200).json({
-      ok: true,
-      user: {
-        id: data.data.id,
-        name: data.data.name,
-        username: data.data.username,
-        profile_image_url: data.data.profile_image_url,
-      }
-    });
-
+    return res.status(200).json({ ok: true, user: data });
   } catch (e) {
-    return res.status(500).json({ ok: false, error: e.message });
+    return res.status(500).json({ error: e.message });
   }
 }
