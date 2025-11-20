2bed274938b80f132d448e45bb9d95181bb0ac98
Bajci2 <peti0618@gmail.com>
2025-11-20 19:53:30 +0000

    Update twitter-callback.js

--- api/twitter-callback.js
+++ api/twitter-callback.js
@@ -1,5 +1,6 @@
 export default async function handler(req, res) {
   const code = req.query.code;
+  const state = req.query.state;
 
   if (!code) {
     return res.status(400).json({ error: "Missing code" });
@@ -9,6 +10,7 @@ export default async function handler(req, res) {
   const CLIENT_SECRET = process.env.TWITTER_CLIENT_SECRET;
   const REDIRECT_URI = "https://catkitsune.xyz/api/twitter-callback";
 
+  // IMPORTANT: Twitter requires Basic Auth for token exchange
   const basicAuth = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString("base64");
 
   try {
@@ -20,7 +22,7 @@ export default async function handler(req, res) {
       },
       body: new URLSearchParams({
         grant_type: "authorization_code",
-        code: code,
+        code,
         redirect_uri: REDIRECT_URI,
         code_verifier: "challenge"
       })
@@ -32,18 +34,17 @@ export default async function handler(req, res) {
       return res.status(400).json({ error: "Failed to obtain access token", token });
     }
 
-    // --- USER DATA ---
-    const userReq = await fetch("https://api.twitter.com/2/users/me?user.fields=profile_image_url,name,username", {
-      headers: {
-        "Authorization": `Bearer ${token.access_token}`,
-      },
-    });
+    // --- Get user info ---
+    const userRequest = await fetch(
+      "https://api.twitter.com/2/users/me?user.fields=profile_image_url,name,username",
+      { headers: { Authorization: `Bearer ${token.access_token}` } }
+    );
 
-    const user = await userReq.json();
+    const userData = await userRequest.json();
 
-    const name = encodeURIComponent(user.data.name);
-    const username = encodeURIComponent(user.data.username);
-    const avatar = encodeURIComponent(user.data.profile_image_url);
+    const name = encodeURIComponent(userData.data.name);
+    const username = encodeURIComponent(userData.data.username);
+    const avatar = encodeURIComponent(userData.data.profile_image_url);
 
     return res.redirect(`https://catkitsune.xyz/?name=${name}&username=${username}&avatar=${avatar}`);
 
